858ae2403190aece0a99f208a3d96fd75b889e26
Antonio Cuadros <antculap99@gmail.com>
2020-11-13 04:22:38 -0800

    Añadida funcion para vercel

--- api/date.rb
+++ api/date.rb
@@ -2,5 +2,5 @@
 Handler = Proc.new do |req, res|
   res.status = 200
   res['Content-Type'] = 'text/text; charset=utf-8'
-  res.body = "Dia y hora actual: #{Time.new}"
+  res.body = "Dia y hora actual: #{Time.now}"
 end
